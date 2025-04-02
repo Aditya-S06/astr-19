b3d5be70d83c95ceaec04c225d32f4f059e02135
Aditya-S06 <118243805+Aditya-S06@users.noreply.github.com>
2025-04-01 19:47:54 -0700

    Session 1 Entry
    first prompt

---
+++ CodeJournal#1_Session1Prompt.py
@@ -0,0 +1,6 @@
+nameTag = "My Name: "
+pronounTag = "My Pronouns: "
+favouritesTag = "My favourite things are: "
+print(f"{nameTag}Aditya Singh")
+print(f"{pronounTag}He/him")
+print(f"{favouritesTag}My favorite movie is Ready Player One and my favorite food is dominos pizza")
