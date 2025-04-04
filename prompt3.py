41dae1c003e1887810e39575118828c9189be46f
Aditya Singh <adityas5609@gmail.com>
2025-04-03 19:25:27 -0700

    prompt3

---
+++ prompt3.py
@@ -0,0 +1,13 @@
+# Defining the funciton 
+def fuction(x):
+    x = x ** 3 + 8
+    print(x)
+    if x > 27:
+        print("YAY!!")
+
+def main():
+    fuction(9)
+
+if __name__=="__main__":
+    main()
+
