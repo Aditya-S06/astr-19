d49bfd6bc7be3cd5abfda52f4a69856fa683124c
Aditya-S06 <118243805+Aditya-S06@users.noreply.github.com>
2025-04-03 19:15:23 -0700

    Code Journal#1 Prompt 2
    Prompt 2 Addition

---
+++ CodeJournal#1_prompt2.py
@@ -0,0 +1,21 @@
+# Tags describing the type of number
+flot = "Floating Point:"
+inte = "Integer:"
+
+# Initializing variables 
+x = 5.12
+y = 832.2
+z = 3
+c = 234
+
+# Equations being done to the variables
+sumOfFloat = x + y
+diffOfInt = c - z
+productOfFloat = x * y
+productOfInt = z * c
+
+# Result
+print(f"Sum of {x} + {y} is equal to {flot}{sumOfFloat}")
+print(f"Difference of {c} - {z} is equal to {inte}{diffOfInt}")
+print(f"Product of a {flot}{productOfFloat}")
+print(f"Product of {inte}{productOfInt}")
